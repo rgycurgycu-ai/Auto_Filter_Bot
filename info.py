52820dc4d48deaa2f01f73b3c4ab6571c4c05e66
rgycurgycu-ai <rgycurgycu@gmail.com>
2025-10-20 04:14:09 -0700

    Update info.py

--- info.py
+++ info.py
@@ -18,9 +18,7 @@ def is_enabled(value, default):
 # Bot Information Configuration
 # ============================
 SESSION = environ.get('SESSION', 'dreamxbotz_search')   # Session name for the bot
-API_ID = int(environ.get('API_ID', '
-21643763
-')) # API ID from my.telegram.org
+API_ID = int(environ.get('API_ID', '21643763')) # API ID from my.telegram.org
 API_HASH = environ.get('API_HASH', '9ab0b0e819951db76417c7e4962ccdf5')  # API Hash from my.telegram.org
 BOT_TOKEN = environ.get('BOT_TOKEN', "8030913096:AAHTjXM3r8T_FVcDytkup7b6oUaN9pPOpug")    # Bot token from @BotFather
 
